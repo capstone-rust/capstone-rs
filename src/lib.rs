a93001f87a85441009cbd5429300da8789b18f08
Richo Healey <richo@psych0tik.net>
2015-04-06 20:30:11 -0700

    Add demo as a test

--- src/lib.rs
+++ src/lib.rs
@@ -22,3 +22,32 @@ pub use capstone::Capstone;
 /// obvious
 #[allow(non_camel_case_types)]
 pub type csh = libc::size_t;
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    static CODE: &'static [u8] = b"\x55\x48\x8b\x05\xb8\x13\x00\x00";
+
+    #[test]
+    fn test_x86_simple() {
+    match capstone::Capstone::new(constants::CsArch::ARCH_X86,
+                                  constants::CsMode::MODE_64) {
+        Some(cs) => {
+            if let Some(insns) = cs.disasm(CODE, 0x1000, 0) {
+                assert_eq!(insns.len(), 2);
+                let is: Vec<_> = insns.iter().collect();
+                assert_eq!(is[0].mnemonic().unwrap(), "push");
+                assert_eq!(is[1].mnemonic().unwrap(), "mov");
+
+                assert_eq!(is[0].address, 0x1000);
+                assert_eq!(is[1].address, 0x1001);
+            } else {
+                assert!(false, "Couldn't disasm instructions")
+            }
+        },
+        None => {
+            assert!(false, "Couldn't create a cs engine");
+        }
+    }
+}
+}
