2de1b29c88065bd9227b7e373fa963dbae1b09d4
Richo Healey <richo@psych0tik.net>
2016-03-07 19:21:49 -0800

    Remove deprecated function

--- src/ffi.rs
+++ src/ffi.rs
@@ -15,13 +15,6 @@ extern "C" {
                      count: libc::size_t,
                      insn: &mut *const Insn)
                      -> libc::size_t;
-    pub fn cs_disasm_ex(handle: csh,
-                        code: *const u8,
-                        code_size: libc::size_t,
-                        address: u64,
-                        count: libc::size_t,
-                        insn: &mut *const Insn)
-                        -> libc::size_t;
     pub fn cs_free(insn: *const Insn, count: libc::size_t);
     pub fn cs_reg_name(handle: csh, reg_id: libc::size_t) -> *const libc::c_char;
     pub fn cs_insn_name(handle: csh, insn_id: libc::size_t) -> *const libc::c_char;
