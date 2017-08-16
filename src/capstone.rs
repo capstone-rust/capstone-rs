e31b42fe1b2760db9eb263494cf356ffc6cf08f6
Travis Finkenauer <tmfinken@gmail.com>
2017-08-16 02:29:00 -0700

    Define arch/mode enums with macros

--- src/capstone.rs
+++ src/capstone.rs
@@ -14,100 +14,120 @@ pub struct Capstone {
     _arch: Arch,
 }
 
-/// Architectures for the disassembler
-#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
-pub enum Arch {
-    ARM,
-    ARM64,
-    MIPS,
-    X86,
-    PPC,
-    SPARC,
-    SYSZ,
-    XCORE,
-    MAX,
+/// Define an `enum` that corresponds to a capstone enum
+///
+/// The different `From` implementations can be disabled by using the cfg attribute
+macro_rules! define_cs_enum_wrapper {
+    ( [
+        $( #[$enum_attr:meta] )*
+        => $rust_enum:ident = $cs_enum:ident,
+        $( #[$cs_to_rust_attrs:meta] )*
+        ,
+        $( #[$rust_to_cs_attrs:meta] )*
+      ]
+      $( $( #[$attr:meta] )*
+      => $rust_variant:ident = $cs_variant:tt; )* ) => {
+
+        $( #[$enum_attr] )*
+        #[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
+        pub enum $rust_enum {
+            $(
+                $( #[$attr] )*
+                $rust_variant,
+            )*
+        }
+
+        $( #[$cs_to_rust_attrs] )*
+        impl From<$cs_enum> for $rust_enum {
+            fn from(other: $cs_enum) -> Self {
+                match other {
+                    $(
+                        $cs_enum::$cs_variant => $rust_enum::$rust_variant,
+                    )*
+                }
+            }
+        }
+
+        $( #[$rust_to_cs_attrs] )*
+        impl From<$rust_enum> for $cs_enum {
+            fn from(other: $rust_enum) -> Self {
+                match other {
+                    $(
+                        $rust_enum::$rust_variant => $cs_variant,
+                    )*
+                }
+            }
+        }
+    }
+}
+
+define_cs_enum_wrapper!(
+    [
+        /// Architectures for the disassembler
+        => Arch = cs_arch,
+        ,
+    ]
+    /// ARM (Advanced RISC Machine)
+    => ARM = CS_ARCH_ARM;
+    /// ARM 64-bit (also known as AArch64)
+    => ARM64 = CS_ARCH_ARM64;
+    /// MIPS
+    => MIPS = CS_ARCH_MIPS;
+    /// x86 family (includes 16, 32, and 64 bit modes)
+    => X86 = CS_ARCH_X86;
+    /// PowerPC
+    => PPC = CS_ARCH_PPC;
+    /// SPARC
+    => SPARC = CS_ARCH_SPARC;
+    /// System z
+    => SYSZ = CS_ARCH_SYSZ;
+    /// XCore
+    => XCORE = CS_ARCH_XCORE;
+    => MAX = CS_ARCH_MAX;
     /// used for `cs_support()`
-    ALL,
-}
+    => ALL = CS_ARCH_ALL;
+);
 
-impl Arch {
-    pub fn to_cs_arch(&self) -> cs_arch {
-        use Arch::*;
-        match *self {
-            ARM => CS_ARCH_ARM,
-            ARM64 => CS_ARCH_ARM64,
-            MIPS => CS_ARCH_MIPS,
-            X86 => CS_ARCH_X86,
-            PPC => CS_ARCH_PPC,
-            SPARC => CS_ARCH_SPARC,
-            SYSZ => CS_ARCH_SYSZ,
-            XCORE => CS_ARCH_XCORE,
-            MAX => CS_ARCH_MAX,
-            ALL => CS_ARCH_ALL,
-        }
-    }
-}
-
-/// Disassembler modes
-#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
-pub enum Mode {
+define_cs_enum_wrapper!(
+    [
+        /// Disassembler modes
+        => Mode = cs_mode,
+        #[cfg(other)]
+        ,
+    ]
     /// little-endian mode (default mode)
-    LittleEndian,
+    => LittleEndian = CS_MODE_LITTLE_ENDIAN;
     /// 32-bit ARM
-    Arm32,
+    => Arm32 = CS_MODE_ARM;
     /// 16-bit mode (X86)
-    Mode16,
+    => Mode16 = CS_MODE_16;
     /// 32-bit mode (X86)
-    Mode32,
+    => Mode32 = CS_MODE_32;
     /// 64-bit mode (X86, PPC)
-    Mode64,
+    => Mode64 = CS_MODE_64;
     /// ARM's Thumb mode, including Thumb-2
-    Thumb,
+    => Thumb = CS_MODE_THUMB;
     /// ARM's Cortex-M series
-    MClass,
+    => MClass = CS_MODE_MCLASS;
     /// ARMv8 A32 encodings for ARM
-    V8,
+    => V8 = CS_MODE_V8;
     /// MicroMips mode (MIPS)
-    Micro,
+    => Micro = CS_MODE_MICRO;
     /// Mips III ISA
-    Mips3,
+    => Mips3 = CS_MODE_MIPS3;
     /// Mips32r6 ISA
-    Mips32R6,
+    => Mips32R6 = CS_MODE_MIPS32R6;
     /// General Purpose Registers are 64-bit wide (MIPS)
-    MipsGP64,
+    => MipsGP64 = CS_MODE_MIPSGP64;
     /// SparcV9 mode (Sparc)
-    V9,
+    => V9 = CS_MODE_V9;
     /// big-endian mode
-    BigEndian,
+    => BigEndian = CS_MODE_BIG_ENDIAN;
     /// Mips32 ISA (Mips)
-    Mips32,
+    => Mips32 = CS_MODE_MIPS32;
     /// Mips64 ISA (Mips)
-    Mips64,
-}
-
-impl Mode {
-    pub fn to_cs_mode(&self) -> capstone_sys::cs_mode {
-        use Mode::*;
-        match *self {
-            LittleEndian => CS_MODE_LITTLE_ENDIAN,
-            Arm32 => CS_MODE_ARM,
-            Mode16 => CS_MODE_16,
-            Mode32 => CS_MODE_32,
-            Mode64 => CS_MODE_64,
-            Thumb => CS_MODE_THUMB,
-            MClass => CS_MODE_MCLASS,
-            V8 => CS_MODE_V8,
-            Micro => CS_MODE_MICRO,
-            Mips3 => CS_MODE_MIPS3,
-            Mips32R6 => CS_MODE_MIPS32R6,
-            MipsGP64 => CS_MODE_MIPSGP64,
-            V9 => CS_MODE_V9,
-            BigEndian => CS_MODE_BIG_ENDIAN,
-            Mips32 => CS_MODE_MIPS32,
-            Mips64 => CS_MODE_MIPS64,
-        }
-    }
-}
+    => Mips64 = CS_MODE_MIPS64;
+);
 
 impl Capstone {
     /// Create a new instance of the decompiler. Defaults to 64-bit little-endian mode.
@@ -119,7 +139,7 @@ impl Capstone {
     /// ```
     pub fn new(arch: Arch) -> Result<Capstone> {
         let mut handle: capstone_sys::csh = 0;
-        let csarch = arch.to_cs_arch();
+        let csarch: cs_arch = arch.into();
         let csmode = CS_MODE_LITTLE_ENDIAN;
         let err = unsafe { cs_open(csarch, csmode, &mut handle) };
         // this can be put into a macro, cs_try, but i guess who cares
@@ -153,7 +173,8 @@ impl Capstone {
     pub fn mode(&mut self, modes: &[Mode]) -> Result<()> {
         let mut value: usize = 0;
         for mode in modes {
-            value |= mode.to_cs_mode() as usize;
+            let mode = cs_mode::from(*mode);
+            value |= mode as usize;
         }
         self.set_option(cs_opt_type::CS_OPT_MODE, value)
     }
