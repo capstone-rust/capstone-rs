f3d1fe3bc671965b6b471e9dd04a81c2ea47694f
Travis Finkenauer <tmfinken@gmail.com>
2017-10-29 02:19:14 -0700

    Refactor demo to print insn/group names
    Demo script only printed register/group ids; not names are printed.

--- examples/demo.rs
+++ examples/demo.rs
@@ -2,7 +2,28 @@ extern crate capstone;
 
 use capstone::prelude::*;
 
-const CODE: &'static [u8] = b"\x55\x48\x8b\x05\xb8\x13\x00\x00";
+const CODE: &'static [u8] =
+    b"\x55\x48\x8b\x05\xb8\x13\x00\x00\xe8\x4a\xed\xff\xff\xe9\x14\x9e\x08\x00\x45\x31\xe4";
+
+/// Print register names
+fn reg_names<T, I>(cs: &Capstone, regs: T) -> String
+where
+    T: Iterator<Item = I>,
+    I: Into<u64>,
+{
+    let names: Vec<String> = regs.map(|x| cs.reg_name(x.into()).unwrap()).collect();
+    names.join(", ")
+}
+
+/// Print instruction group names
+fn group_names<T, I>(cs: &Capstone, regs: T) -> String
+where
+    T: Iterator<Item = I>,
+    I: Into<u64>,
+{
+    let names: Vec<String> = regs.map(|x| cs.group_name(x.into()).unwrap()).collect();
+    names.join(", ")
+}
 
 fn example() -> CsResult<()> {
     let cs = Capstone::new()
@@ -13,12 +34,28 @@ fn example() -> CsResult<()> {
         .build()?;
 
     let insns = cs.disasm_all(CODE, 0x1000)?;
-    println!("Got {} instructions", insns.len());
+    println!("Found {} instructions", insns.len());
     for i in insns.iter() {
+        println!("");
         println!("{}", i);
-        println!("    read regs: {:?}", cs.read_registers(&i).unwrap());
-        println!("    write regs: {:?}", cs.write_registers(&i).unwrap());
-        println!("    insn groups: {:?}", cs.insn_groups(&i).unwrap());
+        let output: &[(&str, String)] =
+            &[
+                (
+                    "read regs:",
+                    reg_names(&cs, cs.read_register_ids(&i)?.iter().map(|x| *x)),
+                ),
+                (
+                    "write regs:",
+                    reg_names(&cs, cs.write_register_ids(&i)?.iter().map(|x| *x)),
+                ),
+                (
+                    "insn groups:",
+                    group_names(&cs, cs.insn_group_ids(&i)?.iter().map(|x| *x)),
+                ),
+            ];
+        for &(ref name, ref message) in output.iter() {
+            println!("    {:12} {}", name, message);
+        }
     }
     Ok(())
 }
