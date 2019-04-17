1df2b209fa98ed9360de39e43728ac485f0445f4
Travis Finkenauer <tmfinken@gmail.com>
2019-04-17 01:10:22 -0400

    Add general tagging script

---
+++ scripts/tag_release.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#
+# Tag the HEAD based on version in Cargo.toml
+
+set -eu
+
+cd "$(dirname $0)/.."
+
+extract_toml_value() {
+    grep "^$1" Cargo.toml | sed 's/^[^ =].*=.*"\([^"]\+\)"$/\1/' | head -n1
+}
+
+PACKAGE_NAME="$(extract_toml_value name)"
+PACKAGE_VERSION="$(extract_toml_value version)"
+DESCRIPTION="${PACKAGE_NAME} v${PACKAGE_VERSION}"
+TAG_NAME="${PACKAGE_NAME}-v${PACKAGE_VERSION}"
+
+echo "Last commit:"
+git log -n1 | cat
+
+echo
+echo -n "Create git tag: TAG_NAME=\"$TAG_NAME\" DESCRIPTION=\"$DESCRIPTION\"? (y/N) "
+
+read -r answer
+case "$answer" in
+    y|Y) ;;
+    *)
+        echo "Exiting"
+        exit 1
+        ;;
+esac
+
+set -x
+git tag -s -m "${DESCRIPTION}" "${TAG_NAME}"
+set +x
+
+echo
+echo "Don't forget to push tags upstream:"
+echo
+echo "    git push origin --tags"
