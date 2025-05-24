da7ec965333c747c5b03a44c7a9261ee895aa6da
Jiajie Chen <c@jia.je>
2025-05-24 09:05:00 +0800

    rs, sys: generate arm_spsr_cspr_bits as bitfield enum

--- capstone-sys/build.rs
+++ capstone-sys/build.rs
@@ -277,7 +277,7 @@ fn write_bindgen_bindings(
         .layout_tests(false) // eliminate test failures on platforms with different pointer sizes
         .impl_debug(true)
         .constified_enum_module("cs_err|cs_group_type|cs_opt_value")
-        .bitfield_enum("cs_mode|cs_ac_type")
+        .bitfield_enum("cs_mode|cs_ac_type|arm_spsr_cspr_bits")
         .rustified_enum(".*")
         .array_pointers_in_arguments(true)
         .no_copy("cs_insn");
